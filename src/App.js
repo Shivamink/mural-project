0f56409fd3d9650388b743018decfa137fa61a2c
Shivam Pandey <shivam.pandey@squarepixelstudios.net>
2022-05-20 22:02:40 +0530

    completed ui part

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Ground from "./Assets/Ground.png";
 import Airport from "./Assets/Airport.png";
 import Building from "./Assets/Building.png";
@@ -18,12 +20,146 @@ import SLRAhole from "./Assets/SLRA hole.png";
 import IMAbuilding from "./Assets/IM&A building.png";
 import TMTDrone from "./Assets/TMT Drone.png";
 import TMTMovieposter from "./Assets/TMT Movie poster.png";
+import TMTStadium from "./Assets/TMT Stadium.png";
+import Mobileqr from "./Assets/Mobile qr.png";
+import Consumermarkets from "./Assets/Consumer markets.png";
+import EnergyNatural from "./Assets/Energy & Natural  Resources.png";
+import FinancialServices from "./Assets/Financial Services.png";
+import GovernmentPublic from "./Assets/Government &  Public Services.png";
+import InfrastructureRealEstate from "./Assets/Infrastructure & Real Estate.png";
+import IndustrialManufacturingAuto from "./Assets/Industrial Manufacturing  & Auto.png";
+import TechnologyMediatelecom from "./Assets/Technology, Media & telecom.png";
+import Services from "./Assets/Services.png";
+import BusinessConsulting from "./Assets/Business Consulting.png";
+import DigitalTrustCyber from "./Assets/Digital Trust- Cyber.png";
+import RiskAdvisory from "./Assets/Risk Advisory.png";
+import TechEnablement from "./Assets/Tech Enablement.png";
+import OneMA from "./Assets/One M&A.png";
+import ManagedServices from "./Assets/Managed Services.png";
+import Tax from "./Assets/Tax.png";
+import EnvironmentGovernance from "./Assets/Environment, Social and Governance.png";
 
 function App() {
+  const [isActive, setActive] = useState(false);
+  const [child, Setchild] = useState(false);
+
+  const [prevHotspotId, setPrevHotspotId] = useState(null);
+  const [currentHotspotId, setCurrentHotspotId] = useState(null);
+
+  // const [airport, setairport] = useState(false);
+  // const [tmtStadium, setmtStadium] = useState(false);
+  // const [cmhospital, setcmhospital] = useState(false);
+  // const [edTech, setedTech] = useState(false);
+  // const [fsbank, setfsbank] = useState(false);
+  // const [iredock, setiredock] = useState(false);
+  // const [irebuilding, setirebuilding] = useState(false);
+
+  function handleblur(event) {
+    // setActive(!isActive);
+    setActive(true);
+    // console.log(event.target.id);
+  }
+
+  // function cm(event) {
+  //   console.log(event.target);
+  //   setcmhospital(true);
+  //   setedTech(true);
+  //   setfsbank(false);
+  // }
+
+  // function fs() {
+  //   setcmhospital(false);
+  //   setedTech(false);
+  //   setfsbank(true);
+  // }
+
+  // function re() {
+  //   setcmhospital(false);
+  //   setedTech(false);
+  //   setfsbank(false);
+  //   setiredock(true);
+  //   setirebuilding(true);
+  // }
+
+  function showbutton() {
+    Setchild(!child);
+  }
+
+  function tmtdisplay() {
+    console.log("hello");
+  }
+
+  let completemap = {
+    sector1: ["CMhospital", "EDTech"],
+    sector2: [""],
+    sector3: ["FSbank"],
+    sector4: [""],
+    sector5: ["IREdock", "IREbuilding"],
+    sector6: ["IMAbuilding"],
+    sector7: [
+      "TMTbuilding",
+      "EDTech",
+      "TMTStadium",
+      "Mobileqr",
+      "SLRAhole",
+      "TMTDrone",
+      "TMTMovieposter",
+    ],
+    service1: ["SLBCcoal", "SLBCbuilding", "Mobileqr"],
+    service2: ["Mobileqr"],
+    service3: ["SLRAhole", "SLRAgovernment"],
+    service4: [""],
+    service5: [""],
+    service6: [""],
+    service7: [""],
+    service8: ["ESGelectric"],
+    service9: [""],
+  };
+
+  function enablegroup(event) {
+    setPrevHotspotId(currentHotspotId);
+    setCurrentHotspotId(event.target.id);
+    console.log({ currentHotspotId, prevHotspotId });
+
+    /**
+     * @type {string[]}
+     */
+    const currentHotspot = completemap[currentHotspotId];
+    if (currentHotspot) {
+      currentHotspot.forEach((h) => {
+        document.getElementById(h).style.zIndex = 1;
+      });
+    }
+
+    /**
+     * @type {string[]}
+     */
+    const prevHotspot = completemap[prevHotspotId];
+    if (prevHotspot) {
+      prevHotspot.forEach((h) => {
+        document.getElementById(h).style.zIndex = 0;
+      });
+    }
+  }
+
+  // let roughArray = [];
+  // const [stateKey, setStateKey] = useState("sector1");
+  // for (let key in completemap) {
+  //   if (key === stateKey) {
+  //     roughArray.push(completemap[key]);
+  //     console.log(completemap[key]);
+  //   }
+  // }
+
+  // const getSectorName = () => {
+  //   return roughArray;
+  // };
+  // console.log(getSectorName());
   return (
     <div className="App">
-      <div id="Parent">
+      <div id="Parent" onClick={handleblur}>
         <img className="bg-image" src={Ground} />
+        <div className={isActive ? "backdrop" : ""}></div>
         <div id="Airport">
           <img className="airport-image" src={Airport} />
         </div>
@@ -39,7 +175,7 @@ function App() {
         <div id="IREbuilding">
           <img className="IREbuilding-image" src={IREbuildings} />
         </div>
-        <div id="TMTbuilding">
+        <div id="TMTbuilding" onClick={showbutton}>
           <img className="TMTbuilding-image" src={TMTbuilding} />
         </div>
         <div id="CMhospital">
@@ -83,6 +219,124 @@ function App() {
         </div>
         <div id="TMTMovieposter2">
           <img className="TMTMovieposter-image" src={TMTMovieposter} />
+        </div>
+        <div id="TMTStadium">
+          <img className="TMTStadium-image" src={TMTStadium} />
+        </div>
+        <div id="Mobileqr">
+          <img className="Mobileqr-image" src={Mobileqr} />
+        </div>
+        <div id="primary">
+          <button className="primary-btn" onClick={enablegroup}>
+            <img
+              className="primary-btnimage"
+              id="sector1"
+              src={Consumermarkets}
+            />
+          </button>
+          <button className="primary-btn" onClick={enablegroup}>
+            <img
+              className="primary-btnimage"
+              id="sector2"
+              src={EnergyNatural}
+            />
+          </button>
+          <button className="primary-btn" onClick={enablegroup}>
+            <img
+              className="primary-btnimage"
+              id="sector3"
+              src={FinancialServices}
+            />
+          </button>
+          <button className="primary-btn" onClick={enablegroup}>
+            <img
+              className="primary-btnimage"
+              id="sector4"
+              src={GovernmentPublic}
+            />
+          </button>
+          <button className="primary-btn" onClick={enablegroup}>
+            <img
+              className="primary-btnimage"
+              id="sector5"
+              src={InfrastructureRealEstate}
+            />
+          </button>
+          <button className="primary-btn" onClick={enablegroup}>
+            <img
+              className="primary-btnimage"
+              id="sector6"
+              src={IndustrialManufacturingAuto}
+            />
+          </button>
+          <button className="primary-btn" onClick={enablegroup}>
+            <img
+              className="primary-btnimage"
+              id="sector7"
+              src={TechnologyMediatelecom}
+            />
+          </button>
+        </div>
+        <div id="service">
+          <button className="service-btn" onClick={enablegroup}>
+            <img src={Services} className="service-btnimage" id="service1" />
+          </button>
+          <button className="service-btn" onClick={enablegroup}>
+            <img
+              src={BusinessConsulting}
+              className="service-btnimage"
+              id="service2"
+            />
+          </button>
+          <button className="service-btn" onClick={enablegroup}>
+            <img
+              src={DigitalTrustCyber}
+              className="service-btnimage"
+              id="service3"
+            />
+          </button>
+          <button className="service-btn" onClick={enablegroup}>
+            <img
+              src={RiskAdvisory}
+              className="service-btnimage"
+              id="service4"
+            />
+          </button>
+          <button className="service-btn" onClick={enablegroup}>
+            <img
+              src={TechEnablement}
+              className="service-btnimage"
+              id="service5"
+            />
+          </button>
+          <button className="service-btn" onClick={enablegroup}>
+            <img src={OneMA} className="service-btnimage" id="service6" />
+          </button>
+          <button className="service-btn" onClick={enablegroup}>
+            <img
+              src={ManagedServices}
+              className="service-btnimage"
+              id="service7"
+            />
+          </button>
+          <button className="service-btn" onClick={enablegroup}>
+            <img src={Tax} className="service-btnimage" id="service8" />
+          </button>
+          <button className="service-btn" onClick={enablegroup}>
+            <img
+              src={EnvironmentGovernance}
+              className="service-btnimage"
+              id="service9"
+            />
+          </button>
+        </div>
+        <div
+          className="childbtns"
+          style={child ? { display: "flex" } : { display: "none" }}
+        >
+          <button className="childbtn">Image</button>
+          <button className="childbtn">Video</button>
+          <button className="childbtn">PDF</button>
         </div>
       </div>
     </div>
